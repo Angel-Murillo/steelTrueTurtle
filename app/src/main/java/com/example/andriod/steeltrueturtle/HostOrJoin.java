22425b83d8f22f47a0438c07eeec74b959297e7c
jerrylee <JerryDeKo@gmail.com>
2017-05-17 12:03:28 -0700

    Minor add ons for the Host or Client Activity. Added some TextViews

--- app/src/main/java/com/example/andriod/steeltrueturtle/HostOrJoin.java
+++ app/src/main/java/com/example/andriod/steeltrueturtle/HostOrJoin.java
@@ -4,14 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class HostOrJoin extends AppCompatActivity {
+
+    private TextView userNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host_or_join);
+
+        userNameTextView = (TextView) findViewById(R.id.userNameTextView);
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(firebaseUser != null){
+            userNameTextView.setText(firebaseUser.getDisplayName());
+        }
 
     }
     public void redirectHost(View view)
